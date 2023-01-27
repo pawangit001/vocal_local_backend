19ec90e25cfe4a62fefcc5dbc4970ad1f935a2da
pawangit001 <pawan.coding001@gmail.com>
2023-01-27 17:47:39 +0530

    changes

--- app.js
+++ app.js
@@ -7,13 +7,13 @@ import cors from "cors";
 
 const app = express();
 dotenv.config();
-
+const PORT = process.env.PORT || 5000
 // middlewares
 app.use(cors());
 app.use(express.json());
 app.use("/user", userRouter);
 app.use("/posts", postRouter);
-const PORT = process.env.PORT || 5000
+
 // connections
 mongoose
   .connect(
@@ -25,4 +25,4 @@ mongoose
     )
   )
   .catch((err) => console.log(err));
-/// password --- QVIyAXmpEtGbamn9
+
