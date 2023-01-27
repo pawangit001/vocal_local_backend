c4f4fa555e62869df50a377bacc167213ed52d27
pawangit001 <pawan.coding001@gmail.com>
2023-01-27 16:50:01 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRouter from "./routing/user-routes";
+import postRouter from "./routing/post-routes";
+import cors from "cors";
+
+const app = express();
+dotenv.config();
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+app.use("/user", userRouter);
+app.use("/posts", postRouter);
+
+// connections
+mongoose
+  .connect(
+    `mongodb+srv://admin:${process.env.MONGODB_PASSWORD}@cluster0.9p3zwm1.mongodb.net/?retryWrites=true&w=majority`
+  )
+  .then(() =>
+    app.listen(5000, () =>
+      console.log("Connection Succesfull  & Listening to localhost Port 5000")
+    )
+  )
+  .catch((err) => console.log(err));
+/// password --- QVIyAXmpEtGbamn9
